8dea89bde5c4b0522cb2605cd5ba1bec58c3d415
Craig Zeise <craig.zeise@gmail.com>
2018-11-21 20:08:45 -0600

    refactor: Move instantiation to before function

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,20 +1,23 @@
 /* eslint-disable no-undef */
 describe('Address Book', function addressBook() {
-  it('should be able to add a contact', function shouldAdd() {
-    var ab = new AddressBook();
-    var thisContact = new Contact();
+  var ab;
+  var thisContact;
 
+  beforeEach(function before() {
+    ab = new AddressBook();
+    thisContact = new Contact();
+  });
+
+  it('should be able to add a contact', function shouldAdd() {
     ab.addContact(thisContact);
 
     expect(ab.getContact(0)).toBe(thisContact);
   });
 
   it('should be able to delete a contact', function shouldDelete() {
-    var ab = new AddressBook();
-    var thisContact = new Contact();
-
     ab.addContact(thisContact);
     ab.deleteContact(0);
+
     expect(ab.getContact(0)).not.toBeDefined();
   });
 });
